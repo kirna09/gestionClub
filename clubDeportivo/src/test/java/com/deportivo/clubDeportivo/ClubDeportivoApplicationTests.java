6a67101338234b8f039f93683d54dadb1af18e11
andrik <assshh351@gmail.com>
2025-04-14 20:44:46 -0700

    primer push

---
+++ clubDeportivo/src/test/java/com/deportivo/clubDeportivo/ClubDeportivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deportivo.clubDeportivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubDeportivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
